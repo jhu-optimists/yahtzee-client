3907af4a8aeb69b419cdaaad5ec37d1c8454a0ec
Joy Hwang <joyhwang10@gmail.com>
2021-07-16 20:49:39 -0400

    wip

--- src/components/ChatArea.js
+++ src/components/ChatArea.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { SocketContext } from '../socket';
 
-export default class ChatArea extends React.Component {
+const ChatArea = () => {
+    const [messages, setMessages] = useState('')
+    const [outgoingMsg, setOutgoing] = useState('')
+    const socket = useContext(SocketContext);
 
-    render() {
-        return(
-            <div>
-                CHAT
-            </div>
-        )
+    const handleServerMessage = (m) => {
+        setMessages(m);
+        console.log(messages);
+    }
+    
+    useEffect(() => {
+        socket.emit("join", "Bob")
+        socket.on("join_success", (msg) => {
+            handleServerMessage(msg);
+        });
+
+        // socket.on("message", (msg) => {  // need listener for general incoming chat messages
+        //     console.log(msg);
+        //     handleServerMessage(msg);
+        // });
+    })
+
+    const handleTyping = (e) => {
+        const newMsg = e.target.value;
+        setOutgoing(newMsg);
     }
 
+    const sendMessage = (e) => {
+        e.preventDefault();
+        socket.emit("message", outgoingMsg);
+    }
+
+    return(
+        <div>
+            <p>{messages}</p>
+            {/* chat entry box below */}
+            {/* <form onSubmit={e => sendMessage(e)}>
+                <input onChange={handleTyping} type="text"></input>
+                <input type="submit" value="Send"></input>
+            </form> */}
+        </div>  
+    )
 }
+
+export default ChatArea
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { SocketContext, socket } from './socket'
+import App from './components/App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SocketContext.Provider value={socket}>
+      <App />
+    </SocketContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
